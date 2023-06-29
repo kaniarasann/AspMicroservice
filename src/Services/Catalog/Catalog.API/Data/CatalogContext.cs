f23e971d384d440c07b10f433594893c5c3e969f
kaniarasann <kaniarasan@outlook.com>
2023-06-28 23:18:03 -0500

    Added basket api basic

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,16 @@
+﻿using Catalog.API.Entites;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config) {
+            var db = new MongoClient(config.GetValue<string>("DatabaseSettings:ConnectionString"))
+                     .GetDatabase(config.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = db.GetCollection<Product>("DatabaseSettings:CollectionName");
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
